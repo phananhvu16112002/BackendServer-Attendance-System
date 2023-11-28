cdc945444232cfdf266f7b1f0b1ed560a4c72659
Kiet <kiettyranno@gmail.com>
2023-11-28 21:27:12 +0700

    Update successfully

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,20 +2,21 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.STUDENT_RESET_TOKEN_SECRET;
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
-
-    console.log("Received token:", token);
-
-    if (!token) {
-        return res.status(403).json({ message: 'Token is not provided' })
-    }
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Token is invalid' })
+    try{
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(403).json({ message: 'Token is not provided' })
         }
-        req.user = decoded;
-        next();
-    });
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: 'Token is invalid' })
+            }
+            req.user = decoded;
+            next();
+        });
+    }catch(e){
+        res.status(500).json({message: "Internal Server"});
+    }
 };
 
 export default verifyToken;
