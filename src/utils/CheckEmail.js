31060d9a0c5566833f7e46fcc4151d4af0daa12e
Kiet <kiettyranno@gmail.com>
2024-03-31 14:46:40 +0700

    update checktime

--- src/utils/CheckEmail.js
+++ src/utils/CheckEmail.js
@@ -1,5 +1,5 @@
 function checkEmailStudent(email) {
-    const regex = /^([a-zA-Z0-9._%+-]+)@student\.tdtu\.edu\.vn$/;
+    const regex = /^([a-zA-Z0-9]+)@student\.tdtu\.edu\.vn$/;
     return regex.test(email);
 }
 
