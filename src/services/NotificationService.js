72beb393cb819f4ba0b14c54aea39aa15d172962
Kiet <kiettyranno@gmail.com>
2024-04-18 22:11:37 +0700

    fix bugs send notification

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -8,7 +8,7 @@ class NotificationService {
         try{
             let {data, error} = await StudentClassService.getStudentsAttendanceDetailsWithDeviceTokenByClassID(classID);
             if (error) {return false};
-            let {passTokens, warningTokens} = getPassTokensAndWarningTokens(data, offset);
+            let {passTokens, warningTokens} = this.getPassTokensAndWarningTokens(data, offset);
             const messageToPassTokens = {
                 notification: {
                     title: "Attendance Form",
@@ -56,7 +56,7 @@ class NotificationService {
     getTokensFromStudentDeviceTokens = (studentDeviceTokens) => {
         let tokens = [];
         for (let i=0; i < studentDeviceTokens.length; i++){
-            tokens.push(studentDeviceTokens.token);
+            tokens.push(studentDeviceTokens[i].token);
         }
         return tokens;
     }
@@ -64,19 +64,17 @@ class NotificationService {
     getPassTokensAndWarningTokens = (studentDetails, offset) => {
         let passTokens = []
         let warningTokens = []
-
         for (let i = 0; i < studentDetails.length; i++){
             let studentDetail = studentDetails[i];
             let status = AttendanceDetailDTO.getStatusBasedOnAttendanceDetails(studentDetail.attendancedetails, offset);
             studentDetail.status = status;
-
+            let tokens = this.getTokensFromStudentDeviceTokens(studentDetail.tokens);
             if (studentDetail.tokens != null && status == "Warning"){
-                warningTokens.push(...studentDetail.tokens);
+                warningTokens.push(...tokens);
             }else if (studentDetail.tokens != null && status == "Pass"){
-                passTokens.push(...studentDetail.tokens);
+                passTokens.push(...tokens);
             }
         }
-
         return {passTokens, warningTokens};
     }
 
