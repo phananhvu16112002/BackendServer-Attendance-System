5324cfb34fd5687b2b9b8e25c4dcfffa3f4d6225
Kiet <kiettyranno@gmail.com>
2024-07-27 22:50:48 +0700

    ActivateService change update method

--- src/services/AttendanceFormService.js
+++ src/services/AttendanceFormService.js
@@ -26,7 +26,6 @@ class AttendanceFormService {
                     location: attendanceForm.location,
                     radius: attendanceForm.radius
                 });
-                save(attendanceForm);
                 await transactionalEntityManager.save(attendanceDetails);
                 
             })
